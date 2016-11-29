65deb3c4806ac73da2f9a119f7c30bdfb2bcbd6a
whgzp <505937370@qq.com>
2016-11-29 14:43:43 +0000

    first commit

---
+++ app/Admin/routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+Route::group([
+    'prefix'        => config('admin.prefix'),
+    'namespace'     => Admin::controllerNamespace(),
+    'middleware'    => ['web', 'admin'],
+], function (Router $router) {
+
+    $router->get('/', 'HomeController@index');
+    $router->get('users', 'UserController@index');
+
+});
